8219acfb2a359769dd4f2f63681aeda9a62a74dd
Alexey Kuznetsov <a.kuznetsov@mercurydevelopment.com>
2023-01-13 21:11:24 +0400

    Fix overflow issue on Android

--- app/screens/AccountHistoryScreen/AccountHistoryScreen.tsx
+++ app/screens/AccountHistoryScreen/AccountHistoryScreen.tsx
@@ -19,14 +19,13 @@ const SCREEN_PADDING_HORIZONTAL = 12
 const $root: ViewStyle = {
   flex: 1,
   backgroundColor: "#523CF8",
-  paddingHorizontal: SCREEN_PADDING_HORIZONTAL,
 }
 
 const $accountCardList: ViewStyle = {
   marginTop: 20,
-  marginHorizontal: -SCREEN_PADDING_HORIZONTAL,
 }
 
 const $recentTransactions: ViewStyle = {
   marginTop: 11,
+  marginHorizontal: SCREEN_PADDING_HORIZONTAL,
 }
