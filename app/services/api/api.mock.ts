1ebcb63e3e6ff765247ea9552c9e8e613c557883
Alexey Kuznetsov <a.kuznetsov@mercurydevelopment.com>
2023-01-18 19:19:56 +0400

    Change mock data to test transactions list updates

--- app/services/api/api.mock.ts
+++ app/services/api/api.mock.ts
@@ -81,45 +81,56 @@ mock.onGet("/accounts").reply<AccountDTO[]>(200, [
   },
 ])
 
-mock.onGet(/\/accounts\/\d+\/transactions/).reply<TransactionDTO[]>(200, [
-  {
-    id: 1,
-    type: "auto",
-    targetName: '"Golub" Taxi Transportation',
-    dateTime: "2022-05-22T18:39:00.000Z",
-    amount: -345,
-    currency: currencyEur,
-  },
-  {
-    id: 2,
-    type: "food",
-    targetName: '"Francois" Restaurant Dinner',
-    dateTime: "2022-05-15T20:56:00.000Z",
-    amount: -1158,
-    currency: currencyEur,
-  },
-  {
-    id: 3,
-    type: "travel",
-    targetName: '"AirMax" Travel to Paris',
-    dateTime: "2022-05-14T16:00:00.000Z",
-    amount: -813,
-    currency: currencyEur,
-  },
-  {
-    id: 4,
-    type: "house",
-    targetName: "Construction ltd.",
-    dateTime: "2022-05-11T09:26:00.000Z",
-    amount: 24500,
-    currency: currencyUsd,
-  },
-  {
-    id: 5,
-    type: "transfer",
-    targetName: "Robert Smith",
-    dateTime: "2022-05-03T12:06:00.000Z",
-    amount: 11215,
-    currency: currencyUsd,
-  },
-])
+let accountTransactionsCounters = {
+  [1]: 1,
+  [2]: 1,
+  [3]: 1,
+}
+mock.onGet(/\/accounts\/\d+\/transactions/).reply<TransactionDTO[]>((config) => {
+  const [, id] = config.url.match(/\/accounts\/(\d+)\/transactions/)
+
+  const transactions = [
+    {
+      id: 1,
+      type: "auto",
+      targetName: '"Golub" Taxi Transportation',
+      dateTime: "2022-05-22T18:39:00.000Z",
+      amount: -345,
+      currency: currencyEur,
+    },
+    {
+      id: 2,
+      type: "food",
+      targetName: '"Francois" Restaurant Dinner',
+      dateTime: "2022-05-15T20:56:00.000Z",
+      amount: -1158,
+      currency: currencyEur,
+    },
+    {
+      id: 3,
+      type: "travel",
+      targetName: '"AirMax" Travel to Paris',
+      dateTime: "2022-05-14T16:00:00.000Z",
+      amount: -813,
+      currency: currencyEur,
+    },
+    {
+      id: 4,
+      type: "house",
+      targetName: "Construction ltd.",
+      dateTime: "2022-05-11T09:26:00.000Z",
+      amount: 24500,
+      currency: currencyUsd,
+    },
+    {
+      id: 5,
+      type: "transfer",
+      targetName: "Robert Smith",
+      dateTime: "2022-05-03T12:06:00.000Z",
+      amount: 11215,
+      currency: currencyUsd,
+    },
+  ]
+
+  return [200, transactions.slice(-accountTransactionsCounters[id]++)]
+})
